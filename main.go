ae4849e0b4e39e88ec1f60dc2b3c2fe91e631980
Farhan Haidar Ar-rasyid <zyonfha286@gmail.com>
2026-05-29 20:41:05 +0700

    dah di tambkan samapi menu yang ke 6(tampil data member)

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+const NMAX = 1000
+
+var nextID int = 1
+
+type member struct {
+	id              string
+	nama            string
+	membership      string
+	durasiMember    int
+	kunjungan       int
+	totalLatihan    int
+	tanggalTerakhir int
+	bulanTerakhir   int
+	tahunTerakhir   int
+}
+
+type tabMember [NMAX]member
+
+type kunjungan struct {
+	idMember string
+	tanggal  string
+	latihan  int
+}
+
+type tabKunjungan [NMAX]kunjungan
+
 func tampilMenu() {
 	fmt.Println()
 	fmt.Println("=====================================================")
@@ -20,7 +46,11 @@ func tampilMenu() {
 }
 
 func main() {
+
 	var pilihan int
+
+	var data tabMember
+	var n int
 
 	for pilihan != 9 {
 
@@ -32,28 +62,22 @@ func main() {
 		switch pilihan {
 
 		case 1:
-			fmt.Println()
-			fmt.Println("===== TAMBAH DATA MEMBER =====")
+			tambahMember(&data, &n)
 
 		case 2:
-			fmt.Println()
-			fmt.Println("===== UBAH DATA MEMBER =====")
+			ubahMember(&data, n)
 
 		case 3:
-			fmt.Println()
-			fmt.Println("===== HAPUS DATA MEMBER =====")
+			hapusMember(&data, &n)
 
 		case 4:
-			fmt.Println()
-			fmt.Println("===== CATAT KUNJUNGAN PENGUNJUNG =====")
+			catatKunjungan(&data, n)
 
 		case 5:
-			fmt.Println()
-			fmt.Println("===== CARI DATA MEMBER =====")
+			cariMember(data, n)
 
 		case 6:
-			fmt.Println()
-			fmt.Println("===== TAMPILKAN DATA MEMBER =====")
+			tampilMember(data, n)
 
 		case 7:
 			fmt.Println()
@@ -71,5 +95,253 @@ func main() {
 			fmt.Println()
 			fmt.Println("Menu tidak tersedia")
 		}
+	}
+}
+
+func tambahMember(A *tabMember, n *int) {
+
+	var pilih int
+
+	fmt.Println()
+	fmt.Println("===== TAMBAH DATA MEMBER =====")
+
+	A[*n].id = fmt.Sprintf("%03d", nextID)
+	nextID++
+
+	fmt.Println("ID Member :", A[*n].id)
+
+	fmt.Print("Nama Member : ")
+	fmt.Scan(&A[*n].nama)
+
+	fmt.Println("Jenis Membership")
+	fmt.Println("1. Basic")
+	fmt.Println("2. Premium")
+	fmt.Print("Pilih : ")
+	fmt.Scan(&pilih)
+
+	if pilih == 1 {
+		A[*n].membership = "Basic"
+	} else {
+		A[*n].membership = "Premium"
+	}
+
+	fmt.Print("Durasi Membership (bulan) : ")
+	fmt.Scan(&A[*n].durasiMember)
+
+	A[*n].kunjungan = 0
+	A[*n].totalLatihan = 0
+
+	*n = *n + 1
+
+	fmt.Println("Data berhasil ditambahkan")
+}
+
+func ubahMember(A *tabMember, n int) {
+
+	var id string
+	var i int
+	var ketemu bool
+	var pilih int
+
+	fmt.Println()
+	fmt.Println("===== UBAH DATA MEMBER =====")
+
+	fmt.Print("Masukkan ID Member : ")
+	fmt.Scan(&id)
+
+	i = 0
+	for i < n && !ketemu {
+
+		if A[i].id == id {
+
+			ketemu = true
+
+			fmt.Print("Nama Baru : ")
+			fmt.Scan(&A[i].nama)
+
+			fmt.Println("Jenis Membership")
+			fmt.Println("1. Basic")
+			fmt.Println("2. Premium")
+			fmt.Print("Pilih : ")
+			fmt.Scan(&pilih)
+
+			if pilih == 1 {
+				A[i].membership = "Basic"
+			} else {
+				A[i].membership = "Premium"
+			}
+
+			fmt.Print("Durasi Baru : ")
+			fmt.Scan(&A[i].durasiMember)
+
+			fmt.Println("Data berhasil diubah")
+		}
+
+		i++
+	}
+
+	if !ketemu {
+		fmt.Println("Member tidak ditemukan")
+	}
+}
+
+func hapusMember(A *tabMember, n *int) {
+
+	var id string
+	var idx int = -1
+	var i int
+
+	fmt.Println()
+	fmt.Println("===== HAPUS DATA MEMBER =====")
+
+	fmt.Print("Masukkan ID Member : ")
+	fmt.Scan(&id)
+
+	for i = 0; i < *n; i++ {
+
+		if A[i].id == id {
+			idx = i
+		}
+	}
+
+	if idx == -1 {
+
+		fmt.Println("Member tidak ditemukan")
+
+	} else {
+
+		for i = idx; i < *n-1; i++ {
+			A[i] = A[i+1]
+		}
+
+		*n = *n - 1
+
+		fmt.Println("Data berhasil dihapus")
+	}
+}
+
+func catatKunjungan(A *tabMember, n int) {
+
+	var id string
+	var durasi int
+	var tanggal, bulan, tahun int
+
+	var i int
+	var ketemu bool
+
+	fmt.Println()
+	fmt.Println("===== CATAT KUNJUNGAN =====")
+
+	fmt.Print("ID Member : ")
+	fmt.Scan(&id)
+
+	for i = 0; i < n && !ketemu; i++ {
+
+		if A[i].id == id {
+
+			ketemu = true
+
+			fmt.Print("Tanggal (dd mm yyyy) : ")
+			fmt.Scan(&tanggal, &bulan, &tahun)
+
+			if A[i].tahunTerakhir != 0 {
+
+				if tahun < A[i].tahunTerakhir {
+
+					fmt.Println("Tanggal tidak valid")
+					fmt.Println("Tahun tidak boleh lebih kecil dari kunjungan terakhir")
+					return
+
+				} else if tahun == A[i].tahunTerakhir &&
+					bulan < A[i].bulanTerakhir {
+
+					fmt.Println("Tanggal tidak valid")
+					fmt.Println("Bulan tidak boleh lebih kecil dari kunjungan terakhir")
+					return
+
+				} else if tahun == A[i].tahunTerakhir &&
+					bulan == A[i].bulanTerakhir &&
+					tanggal < A[i].tanggalTerakhir {
+
+					fmt.Println("Tanggal tidak valid")
+					fmt.Println("Tanggal tidak boleh lebih kecil dari kunjungan terakhir")
+					return
+				}
+			}
+
+			fmt.Print("Durasi Latihan (menit) : ")
+			fmt.Scan(&durasi)
+
+			A[i].kunjungan++
+			A[i].totalLatihan += durasi
+
+			A[i].tanggalTerakhir = tanggal
+			A[i].bulanTerakhir = bulan
+			A[i].tahunTerakhir = tahun
+
+			fmt.Println("Kunjungan berhasil dicatat")
+		}
+	}
+
+	if !ketemu {
+		fmt.Println("Member tidak ditemukan")
+	}
+}
+
+func cariMember(A tabMember, n int) {
+
+	var nama string
+	var i int
+	var ketemu bool
+
+	fmt.Println()
+	fmt.Println("===== CARI DATA MEMBER =====")
+
+	fmt.Print("Masukkan Nama : ")
+	fmt.Scan(&nama)
+
+	for i = 0; i < n; i++ {
+
+		if A[i].nama == nama {
+
+			ketemu = true
+
+			fmt.Println()
+			fmt.Println("Data Ditemukan")
+			fmt.Println("ID         :", A[i].id)
+			fmt.Println("Nama       :", A[i].nama)
+			fmt.Println("Membership :", A[i].membership)
+			fmt.Println("Durasi     :", A[i].durasiMember)
+			fmt.Println("Kunjungan  :", A[i].kunjungan)
+		}
+	}
+
+	if !ketemu {
+		fmt.Println("Data tidak ditemukan")
+	}
+}
+
+func tampilMember(A tabMember, n int) {
+
+	var i int
+
+	fmt.Println()
+	fmt.Println("=========================================================================================================")
+	fmt.Printf("%-6s %-15s %-12s %-8s %-10s %-15s %-15s\n",
+		"ID", "Nama", "Member", "Durasi", "Kunjungan", "Total Latihan", "Tgl Terakhir")
+	fmt.Println("=========================================================================================================")
+
+	for i = 0; i < n; i++ {
+
+		fmt.Printf("%-6s %-15s %-12s %-8d %-10d %-15s %02d/%02d/%04d\n",
+			A[i].id,
+			A[i].nama,
+			A[i].membership,
+			A[i].durasiMember,
+			A[i].kunjungan,
+			fmt.Sprintf("%d Menit", A[i].totalLatihan),
+			A[i].tanggalTerakhir,
+			A[i].bulanTerakhir,
+			A[i].tahunTerakhir)
 	}
 }
