4e29c963a078eaa5f6c73da97b6d62f182c085fe
Farhan Haidar Ar-rasyid <zyonfha286@gmail.com>
2026-05-29 15:02:02 +0700

    menu

--- main.go
+++ main.go
@@ -2,6 +2,60 @@ package main
 
 import "fmt"
 
+func menu() {
+	fmt.Println("=================================================")
+	fmt.Println("        APLIKASI MANAJEMEN GYM MEMBERSHIP")
+	fmt.Println("=================================================")
+	fmt.Println("1. Input Data Member")
+	fmt.Println("2. Input Data Pendatang")
+	fmt.Println("3. Tampilkan Data Pengunjung")
+	fmt.Println("4. Tampilkan Pengunjung Paling Aktif")
+	fmt.Println("5. Keluar")
+	fmt.Println("=================================================")
+}
+
 func main() {
-	fmt.Println("hallo")
+
+	var pilihan int
+
+	for pilihan != 5 {
+
+		menu()
+
+		fmt.Print("Pilih Menu : ")
+		fmt.Scan(&pilihan)
+
+		switch pilihan {
+
+		case 1:
+			fmt.Println()
+			fmt.Println("===== INPUT DATA MEMBER =====")
+			fmt.Println()
+
+		case 2:
+			fmt.Println()
+			fmt.Println("===== INPUT DATA PENDATANG =====")
+			fmt.Println()
+
+		case 3:
+			fmt.Println()
+			fmt.Println("===== TAMPILKAN DATA PENGUNJUNG =====")
+			fmt.Println()
+
+		case 4:
+			fmt.Println()
+			fmt.Println("===== PENGUNJUNG PALING AKTIF =====")
+			fmt.Println()
+
+		case 5:
+			fmt.Println()
+			fmt.Println("Terima kasih telah menggunakan program")
+			fmt.Println()
+
+		default:
+			fmt.Println()
+			fmt.Println("Menu tidak tersedia")
+			fmt.Println()
+		}
+	}
 }
