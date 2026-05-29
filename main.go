a5f57b72cc223ee0398f2897024ac90025385a1b
Farhan Haidar Ar-rasyid <zyonfha286@gmail.com>
2026-05-29 15:15:11 +0700

    tambahan menu

--- main.go
+++ main.go
@@ -2,60 +2,74 @@ package main
 
 import "fmt"
 
-func menu() {
-	fmt.Println("=================================================")
-	fmt.Println("        APLIKASI MANAJEMEN GYM MEMBERSHIP")
-	fmt.Println("=================================================")
-	fmt.Println("1. Input Data Member")
-	fmt.Println("2. Input Data Pendatang")
-	fmt.Println("3. Tampilkan Data Pengunjung")
-	fmt.Println("4. Tampilkan Pengunjung Paling Aktif")
-	fmt.Println("5. Keluar")
-	fmt.Println("=================================================")
+func tampilMenu() {
+	fmt.Println()
+	fmt.Println("=====================================================")
+	fmt.Println("         APLIKASI MANAJEMEN GYM MEMBERSHIP")
+	fmt.Println("=====================================================")
+	fmt.Println("1. Tambah Data Member")
+	fmt.Println("2. Ubah Data Member")
+	fmt.Println("3. Hapus Data Member")
+	fmt.Println("4. Catat Kunjungan Pengunjung")
+	fmt.Println("5. Cari Data Member")
+	fmt.Println("6. Tampilkan Data Member")
+	fmt.Println("7. Urutkan Data Member")
+	fmt.Println("8. Tampilkan Top 3 Member Paling Aktif")
+	fmt.Println("9. Keluar")
+	fmt.Println("=====================================================")
 }
 
 func main() {
-
 	var pilihan int
 
-	for pilihan != 5 {
+	for pilihan != 9 {
 
-		menu()
+		tampilMenu()
 
-		fmt.Print("Pilih Menu : ")
+		fmt.Print("Pilih menu : ")
 		fmt.Scan(&pilihan)
 
 		switch pilihan {
 
 		case 1:
 			fmt.Println()
-			fmt.Println("===== INPUT DATA MEMBER =====")
-			fmt.Println()
+			fmt.Println("===== TAMBAH DATA MEMBER =====")
 
 		case 2:
 			fmt.Println()
-			fmt.Println("===== INPUT DATA PENDATANG =====")
-			fmt.Println()
+			fmt.Println("===== UBAH DATA MEMBER =====")
 
 		case 3:
 			fmt.Println()
-			fmt.Println("===== TAMPILKAN DATA PENGUNJUNG =====")
-			fmt.Println()
+			fmt.Println("===== HAPUS DATA MEMBER =====")
 
 		case 4:
 			fmt.Println()
-			fmt.Println("===== PENGUNJUNG PALING AKTIF =====")
-			fmt.Println()
+			fmt.Println("===== CATAT KUNJUNGAN PENGUNJUNG =====")
 
 		case 5:
 			fmt.Println()
-			fmt.Println("Terima kasih telah menggunakan program")
+			fmt.Println("===== CARI DATA MEMBER =====")
+
+		case 6:
 			fmt.Println()
+			fmt.Println("===== TAMPILKAN DATA MEMBER =====")
+
+		case 7:
+			fmt.Println()
+			fmt.Println("===== URUTKAN DATA MEMBER =====")
+
+		case 8:
+			fmt.Println()
+			fmt.Println("===== TOP 3 MEMBER PALING AKTIF =====")
+
+		case 9:
+			fmt.Println()
+			fmt.Println("Terima kasih telah menggunakan program")
 
 		default:
 			fmt.Println()
 			fmt.Println("Menu tidak tersedia")
-			fmt.Println()
 		}
 	}
 }
